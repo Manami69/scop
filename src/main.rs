332a879801e2231af1a1318b7f114306ac8459fc
Manami69 <lolopez@student.42lyon.fr>
2022-02-23 19:59:25 +0100

    tuto texture OK D'OKAY

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::ffi::{CStr, CString};
 pub mod render_gl;
 use render_gl::{shader::*, program::*, window::Window};
+use image::io::Reader as ImageReader;
+use std::io::Cursor;
+use std::io;
 pub mod env;
 
-fn main() {
+fn main() -> Result<(), io::Error> {
     let (mut event_pump, window, video_subsystem) = Window::new();
 
     let gl_context = window.gl_create_context().unwrap();
@@ -25,17 +28,45 @@ fn main() {
 	let shader_program = Program::from_shaders(
 		&[vert_shader, frag_shader]
 	).unwrap();
+    ////// TEST
+    let mut texture = 0;
+    unsafe {
 
+        gl::GenTextures(1, &mut texture);
+        gl::BindTexture(gl::TEXTURE_2D, texture); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+        // set the texture wrapping parameters
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::REPEAT as i32); // set texture wrapping to gl::REPEAT (default wrapping method)
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::REPEAT as i32);
+        // set texture filtering parameters
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as i32);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
+    }
+        // load image, create texture and generate mipmaps
+        let img = image::open("wall.jpg").unwrap();
+        let data = img.to_rgb8().into_raw();
+    unsafe {
+           gl::TexImage2D(gl::TEXTURE_2D,
+                       0,
+                       gl::RGB as i32,
+                       img.width() as i32,
+                       img.height() as i32,
+                       0,
+                       gl::RGB,
+                       gl::UNSIGNED_BYTE,
+                       &data[0] as *const u8 as *const gl::types::GLvoid);
+        gl::GenerateMipmap(gl::TEXTURE_2D);
+    }
+    /////
 	shader_program.set_used();
 	let vertices: Vec<f32> = vec![
 	// 	x, 		y,  	z,   	r,	b,	g
-		0.5, 	-0.5, 	0.0, 	1., 0., 0.,   // bottom right
-    	-0.5, 	-0.5, 	0.0, 	0.,	1.,	0.,  // bottom let
-    	0.0,  	0.5, 	0.0, 	0.,	0.,	1.,   // top 
+		0.5, 	-0.5, 	0.0, 	1., 0., 0., 0., 0.,  // bottom right
+    	-0.5, 	-0.5, 	0.0, 	0.,	1.,	0.,  1., 0.,// bottom let
+    	0.0,  	0.5, 	0.0, 	0.,	0.,	1., 0.5, 1.,  // top 
 
-		0.5, 	0.8, 	0.0, 	1., 0., 0.,
-		0.0, 	0.8, 	0.0, 	0.,	1.,	0.,
-		0.0, 	0.5, 	0.0, 	0.,	0.,	1.,
+		// 0.5, 	0.8, 	0.0, 	1., 0., 0.,
+		// 0.0, 	0.8, 	0.0, 	0.,	1.,	0.,
+		// 0.0, 	0.5, 	0.0, 	0.,	0.,	1.,
         
 	];
 
@@ -67,7 +98,7 @@ fn main() {
 			3, // the number of components per generic vertex attribute
 			gl::FLOAT, // data type
 			gl::FALSE, // normalized (int-to-float conversion)
-			(6 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
+			(8 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
 			std::ptr::null() // offset of the first component
 		);
         gl::EnableVertexAttribArray(1); // this is "layout (location = 0)" in vertex shader
@@ -76,8 +107,17 @@ fn main() {
 			3, // the number of components per generic vertex attribute
 			gl::FLOAT, // data type
 			gl::FALSE, // normalized (int-to-float conversion)
-			(6 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
+			(8 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
 			(3 * std::mem::size_of::<f32>()) as *const gl::types::GLvoid // offset of the first component
+		);
+        gl::EnableVertexAttribArray(2); // this is "layout (location = 0)" in vertex shader
+		gl::VertexAttribPointer(
+			2, // index of the generic vertex attribute ("layout (location = 0)")
+			2, // the number of components per generic vertex attribute
+			gl::FLOAT, // data type
+			gl::FALSE, // normalized (int-to-float conversion)
+			(8 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
+			(6 * std::mem::size_of::<f32>()) as *const gl::types::GLvoid // offset of the first component
 		);
 	}
     'main: loop {
@@ -91,6 +131,9 @@ fn main() {
             gl::Clear(gl::COLOR_BUFFER_BIT);
         }
 		unsafe {
+            gl::BindTexture(gl::TEXTURE_2D, texture);
+            gl::BindVertexArray(vao);
+
 		    gl::DrawArrays(
 		        gl::TRIANGLES, // mode
 		        0, // starting index in the enabled arrays
@@ -100,4 +143,5 @@ fn main() {
         window.gl_swap_window();
         // render window contents here
     }
+    Ok(())
 }
--- src/triangle.frag
+++ src/triangle.frag
@@ -2,7 +2,9 @@
 
 out vec4 Color;
 in vec4 vertexColor; // the input variable from the vertex shader (same name and same type)  
+in vec2 TexCoord;
+uniform sampler2D ourTexture;
 void main()
 {
-    Color = vertexColor;
+    Color = texture(ourTexture, TexCoord);
 }
