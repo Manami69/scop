15dbab7f5536934d99d552302cc47d1112c92de5
Manami69 <lolopez@student.42lyon.fr>
2022-03-08 18:39:47 +0100

    lol

--- src/render_gl/vbo.rs
+++ src/render_gl/vbo.rs
@@ -2,6 +2,31 @@ pub struct Vbo {
 	id : gl::types::GLuint,
 }
 
+impl Vbo {
+	pub fn new() -> Self{
+		todo!()
+	}
+
+	pub fn use(&self) {
+		gl::BindBuffer(gl::ARRAY_BUFFER, self.id() as gl::types::GLuint);
+	}
+	
+	/// ATTRIB
+	/// 
+	/// * `layout` : location in vertex shader
+	/// 
+	/// * `stride` : the number of components per generic vertex attribute
+	/// 
+	/// * `offset` : offset of the first component
+	pub fn attrib(layout: usize, stride: usize, offset: usize) {
+		todo!();
+	
+	}
+	pub fn id(&self) -> gl::types::GLuint {
+		self.id
+	}
+}
+
 impl Drop for Vbo {
     fn drop(&mut self) {
         unsafe {
